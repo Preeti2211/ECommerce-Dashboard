a73fc1113cd2a9b6db27a0114df85b7f1057e7cb
Preeti2211 <ps9685185@gmail.com>
2024-04-17 14:20:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>e com dashboard project</h1>
+      <Button>buy now</Button>
+      <button>buy now</button>
+    </div>
+  );
+}
+
+export default App;
